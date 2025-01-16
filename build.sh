0255d04b743a8e57ef79e0b5de5c4042cf24a53a
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-01-16 18:15:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+source /path/to/your/venv/bin/activate
+
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
+echo "Migrations Successfully Completed"
