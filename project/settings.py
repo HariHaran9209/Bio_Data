8881abef3eccfb8b555b856a86c57e1708e8420f
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-01-16 15:52:21 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "biodata/templates"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
