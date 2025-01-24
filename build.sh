f356ae53716e9f1ead915fbab09d320722b56e3d
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-01-25 01:39:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ echo "Migrations starts"
 python manage.py makemigrations
 python manage.py makemigrations biodata
 echo "Migrations Successfully Completed"
-python manage.py migrate --fake-initial
+python manage.py migrate biodata zero
+python manage.py migrate biodata
 echo "migrate Successfully Completed"
