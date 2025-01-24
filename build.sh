703d753fb92fe9a59a1034a929dcbf05d890aa8f
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-01-25 01:45:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,5 @@ echo "Migrations starts"
 python manage.py makemigrations
 python manage.py makemigrations biodata
 echo "Migrations Successfully Completed"
-python manage.py migrate biodata zero
 python manage.py migrate biodata
 echo "migrate Successfully Completed"
