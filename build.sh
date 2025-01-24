b956e92c69c4b0246289d89e3314946b69289a48
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-01-25 01:13:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@ set -o errexit
 
 pip install -r requirements.txt
 echo "Migrations starts"
+python manage.py makemigrations
 python manage.py makemigrations biodata
 echo "Migrations Successfully Completed"
-python manage.py migrate
+python manage.py migrate --fake-initial
 echo "migrate Successfully Completed"
