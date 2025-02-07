d7ac467cf84dfe6d9ac43ed5f7c883daf986468a
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-02-07 21:16:50 +0530

    Update 0001_initial.py

--- biodata/migrations/0001_initial.py
+++ biodata/migrations/0001_initial.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0011_update_proxy_permissions'),
+        ('admin', '0001_initial'),
     ]
 
     operations = [
