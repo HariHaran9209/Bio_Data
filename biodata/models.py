5c4349d9d601e92404506caded25281d435954b1
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-02-07 20:20:54 +0530

    Update models.py

--- biodata/models.py
+++ biodata/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import AbstractBaseUser , BaseUser Manager, PermissionsMixin
 
 # Create your models here.
 class Student(models.Model):
@@ -36,3 +37,34 @@ class CustomUserModel(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=25)
     name = models.CharField(max_length=125)
+    
+class CustomUser Manager(BaseUser Manager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser Model(AbstractBaseUser , PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUser Manager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # Add any additional fields that are required for creating a user
+
+    def __str__(self):
+        return self.email
