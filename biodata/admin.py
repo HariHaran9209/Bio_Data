6c2f56b432432aec58bcf60cc6efaf2689304af1
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-02-07 19:49:34 +0530

    Update admin.py

--- biodata/admin.py
+++ biodata/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import *
 
-class UserDataAdmin(UserAdmin):
-    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'role')
-
 # Register your models here.
-admin.site.register(BioData)
-admin.site.register(UserData, UserDataAdmin)
-admin.site.register(Index)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'grade', 'dob', 'stream', 'admission_number')
+    prepopulated_fields = {"slug": ("name", "admission_number")}
+
+admin.site.register(Student, StudentAdmin)
