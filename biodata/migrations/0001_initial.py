c814123e4a9806effba20fcef5d028590326d69a
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-02-08 10:29:40 +0530

    Update 0001_initial.py

--- biodata/migrations/0001_initial.py
+++ biodata/migrations/0001_initial.py
@@ -10,27 +10,9 @@ class Migration(migrations.Migration):
     dependencies = []
 
     operations = [
-        migrations.CreateModel(
-            name='Student',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=125, null=True)),
-                ('age', models.IntegerField(null=True)),
-                ('admission_number', models.IntegerField(null=True)),
-                ('grade', models.CharField(max_length=10, null=True)),
-                ('phone_number', models.CharField(max_length=15, null=True)),
-                ('dob', models.CharField(max_length=25, null=True)),
-                ('emailid', models.EmailField(max_length=254, null=True)),
-                ('stream', models.CharField(max_length=25, null=True)),
-                ('mother_name', models.CharField(max_length=125, null=True)),
-                ('father_name', models.CharField(max_length=125, null=True)),
-                ('aadhar_number', models.IntegerField(null=True)),
-                ('address', models.CharField(max_length=125, null=True)),
-                ('pincode', models.IntegerField(null=True)),
-                ('alt_phone_number', models.CharField(max_length=25, null=True)),
-                ('blood_group', models.CharField(max_length=15, null=True)),
-                ('height', models.CharField(max_length=15, null=True)),
-                ('weight', models.CharField(max_length=15, null=True)),
-            ],
+        migrations.AddField(
+            model_name='Student',
+            name='idle',
+            field=models.CharField(max_length=25, null=True),
         ),
     ]
