1062a5dbf6954b03654b27deda58e09d46839b22
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-02-08 16:24:37 +0530

    Update models.py

--- biodata/models.py
+++ biodata/models.py
@@ -1,48 +1,9 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
-# Manager for CustomUser
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, password, **extra_fields)
-
-# Custom User Model
-class customeruser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=30, blank=True)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    pass
-    
-    objects = CustomUserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []  # Add additional required fields if needed
-
-    def __str__(self):
-        return self.email
-
-# Student Model
-class Student(models.Model):
+# Create your models here.
+class studentdata(models.Model):
     name = models.CharField(max_length=125, null=True)
     age = models.IntegerField(null=True)
     admission_number = models.IntegerField(null=True)
@@ -62,12 +23,19 @@ class Student(models.Model):
     weight = models.CharField(max_length=15, null=True)
     slug = models.SlugField(unique=True, blank=True)
     student_photo = models.ImageField(upload_to='student_photo/', default='student_photo/default.jpg')
-    nothing = models.CharField(max_length=5, null=True, blank=True)
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.admission_number)
-        super(Student, self).save(*args, **kwargs)
+        super(studentdata, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
+    
+class teacherdata(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    teachername = models.CharField(max_length=125, null=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.user.username
