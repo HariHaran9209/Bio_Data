4ae0fca087ac4a6aa0bee252a335ea24fd8025a5
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-02-08 12:15:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 set -o errexit
 
 pip install -r requirements.txt
+echo "Showing Migrations"
+python manage.py showmigrations
 echo "Flushing The Database"
 python manage.py flush --noinput
+echo "Executing SQL Flush"
+python manage.py sqlflush
 echo "Migrations starts"
 python manage.py makemigrations biodata
 echo "Migrations Successfully Completed"
