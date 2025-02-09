c338a1166aee3cfed30f89a9945501dcde9a9b46
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-02-09 16:46:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-python manage.py makemigrations
+python manage.py makemigrations biodata
 echo "Migrations Successfully Completed"
 
 # Apply new migrations
 echo "Migrate Successfully Completed"
-python manage.py migrate
+python manage.py migrate biodata
 
 # Show Migration
 python manage.py showmigrations
