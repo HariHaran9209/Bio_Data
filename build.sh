a9d6523eadb9747879d3e8847fb89200f2762788
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-02-09 15:56:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Delete All The Migrations Except __init__.py
-rm biodata/migrations/0*.py
-
 # Generate migrations if any new model changes exist
 echo "Migrations starts"
 python manage.py makemigrations biodata
